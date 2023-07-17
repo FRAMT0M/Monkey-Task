2bcf28e241b8c068e42fa7972b9d052ac06f52e4
FRAMT0M <115100583+FRAMT0M@users.noreply.github.com>
2023-07-17 11:36:00 -0500

    avances

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ListService } from '../list.service';
 
@@ -7,43 +7,33 @@ import { ListService } from '../list.service';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   tasks: any[] = [];
 
   constructor(private router: Router, private listService: ListService) {}
 
   ngOnInit() {
-    // Obtener las tareas del servicio
+    this.getTasks();
+  }
+
+  getTasks() {
     this.tasks = this.listService.getTasks();
   }
 
-  getStatusColor(status: string) {
+  getTaskColor(status: string) {
     switch (status) {
-      case 'Pendiente':
-        return 'warning';
-      case 'En Proceso':
-        return 'primary';
       case 'Completado':
         return 'success';
+      case 'Pendiente':
+        return 'danger';
+      case 'En Proceso':
+        return 'warning';
       default:
-        return 'medium';
+        return '';
     }
   }
 
-  getCompletedTasksCount() {
-    return this.tasks.filter(task => task.status === 'Completado').length;
-  }
-
-  getPendingTasksCount() {
-    return this.tasks.filter(task => task.status === 'Pendiente').length;
-  }
-
-  getInProgressTasksCount() {
-    return this.tasks.filter(task => task.status === 'En Proceso').length;
-  }
-
-  addTask() {
-    // Redireccionar a la página de agregar/editar tarea
+  navigateToAddEditItemPage() {
     this.router.navigate(['/add-edit-item']);
   }
 }
